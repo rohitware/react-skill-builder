336a48111ef32d57faeba141f1b68336407af273
Rohit Ware <rohitware99@gmail.com>
2025-10-04 18:51:52 +0530

    variable inside JSX

--- src/jsx/JsxExpressions.jsx
+++ src/jsx/JsxExpressions.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 function JsxExpressions() {
+  const hp = 218 * 1.36;
+
   return (
     <div>
       <h1>My car</h1>
-      <p>It has {218 * 1.36} horsepower</p>
+      <p>It has {hp} horsepower</p>
     </div>
   );
 }
