69bf7603dafdde31e0ff2859ac74f44b106e0364
Rohit Ware <rohitware99@gmail.com>
2025-10-04 18:50:09 +0530

    JavaScript expression inside JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // import './App.css'
 import Demo1 from "./jsx/Demo1";
+import JsxExpressions from "./jsx/JsxExpressions";
 
 function App() {
   return (
     <>
       <div className="App">
         <Demo1 />
+        <JsxExpressions />
       </div>
     </>
   );
---
+++ src/jsx/JsxExpressions.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function JsxExpressions() {
+  return (
+    <div>
+      <h1>My car</h1>
+      <p>It has {218 * 1.36} horsepower</p>
+    </div>
+  );
+}
+
+export default JsxExpressions;
