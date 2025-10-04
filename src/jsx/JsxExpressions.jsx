874353f13ccf675b75641cb3043f9b13adf5c32f
Rohit Ware <rohitware99@gmail.com>
2025-10-04 19:02:03 +0530

    function inside JSX:

--- src/jsx/JsxExpressions.jsx
+++ src/jsx/JsxExpressions.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 
+// 🔹 Utility function
+function kwtohp(kw) {
+  return kw * 1.36;
+}
+
 function JsxExpressions() {
-  const hp = 218 * 1.36;
+  //   const hp = 218 * 1.36;
+  //   const hp = kwtohp(218); // use the utility function
 
   return (
     <div>
       <h1>My car</h1>
-      <p>It has {hp} horsepower</p>
+      <p>It has {kwtohp(218)} horsepower</p>
     </div>
   );
 }
